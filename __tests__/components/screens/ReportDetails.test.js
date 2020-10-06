edce700b8367ebc6206e21c2b27f5b04aa6bd31b
Azowyl <facugerstner_29@hotmail.com>
2020-10-06 20:46:56 -0300

    setup tests

---
+++ __tests__/components/screens/ReportDetails.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import ReportDetails from '../../../src/components/screens/report_details';
+
+describe('ReportDetails', () => {
+  let render;
+  let props;
+
+  beforeEach(() => {
+    props = {
+      title: 'a report',
+      description: 'some description',
+    };
+    render = (appProps) => shallow(<ReportDetails {...appProps} />);
+  });
+
+  it('renders without crashing', () => {
+    expect(() => render(props)).not.toThrowError();
+  });
+
+  it('renders all values correctly', () => {
+    expect(render(props)).toMatchSnapshot();
+  });
+});
