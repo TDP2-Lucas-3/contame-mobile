afa82a6853851ad1cf9fb1639fc4ee3c2a7720af
Lucas Lavandeira <llavandeira@devartis.com>
2020-10-14 18:51:36 -0300

    Add permanent marker to map center

--- src/components/screens/reports/new/steps/incident_location_map_view.js
+++ src/components/screens/reports/new/steps/incident_location_map_view.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Button} from 'react-native';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 import {styles} from '../../../../../styles/common';
+import MapMarker from 'react-native-maps/lib/components/MapMarker';
+
+const INITIAL_CENTER = {
+  latitude: 37.78825,
+  longitude: -122.4324,
+};
 
 const IncidentLocationMapViewStep = (props) => {
+  const [center, setCenter] = useState({...INITIAL_CENTER});
+
   return (
     <View>
       <MapView
         provider={PROVIDER_GOOGLE}
         style={styles.map}
-        region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+        initialRegion={{
+          ...INITIAL_CENTER,
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
         }}
-      />
+        onRegionChange={(region) => (region ? setCenter(region) : null)}>
+        <MapMarker coordinate={center} />
+      </MapView>
       <Button title="Seleccionar" onPress={props.onSelect} />
     </View>
   );
