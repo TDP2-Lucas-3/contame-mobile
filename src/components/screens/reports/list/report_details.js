30e62567c7738d6936b9ebd4367b3db47d93347a
Lucas Lavandeira <llavandeira@devartis.com>
2020-11-01 20:42:00 -0300

    Add report list status badge

--- src/components/screens/reports/list/report_details.js
+++ src/components/screens/reports/list/report_details.js
@@ -1,39 +1,55 @@
 import moment from 'moment';
 import React from 'react';
 import {View, TouchableOpacity} from 'react-native';
-import {Card, Text, Icon} from 'react-native-elements';
+import {Card, Text, Icon, Badge} from 'react-native-elements';
 import {styles} from '../../../../styles/common';
+import {verboseReportState} from '../../../../utils/verbose_report_names';
 
-const ReportDetails = (props) => (
-  <TouchableOpacity onPress={props.onPress}>
-    <Card containerStyle={styles.report_card_container}>
-      {props.images.length > 0 ? (
-        <Card.Image
-          testID="card_image"
-          source={{uri: props.images[0]}}
-          style={styles.borderTopRadius_1}
-        />
-      ) : (
-        <Icon
-          name="image-not-supported"
-          testID="default_icon"
-          type="material"
-          containerStyle={styles.report_default_icon_container}
-        />
-      )}
-      <View style={styles.report_card_body}>
-        <View>
-          <Card.Title style={styles.alignSelfStart}>{props.title}</Card.Title>
-          <Text style={styles.color_grey}>
-            {moment(props.creationDate).fromNow()}
-          </Text>
+const ReportDetails = (props) => {
+  console.log(props);
+  return (
+    <TouchableOpacity onPress={props.onPress}>
+      <Card containerStyle={styles.report_card_container}>
+        {props.images.length > 0 ? (
+          <Card.Image
+            testID="card_image"
+            source={{uri: props.images[0]}}
+            style={styles.borderTopRadius_1}>
+            <Badge
+              badgeStyle={styles.report_status_badge}
+              status={'warning'}
+              value={verboseReportState(props.state)}
+            />
+          </Card.Image>
+        ) : (
+          <View>
+            <Icon
+              name="image-not-supported"
+              testID="default_icon"
+              type="material"
+              containerStyle={styles.report_default_icon_container}
+            />
+            <Badge
+              badgeStyle={styles.report_status_badge_no_icon}
+              status={'warning'}
+              value={verboseReportState(props.state)}
+            />
+          </View>
+        )}
+        <View style={styles.report_card_body}>
+          <View>
+            <Card.Title style={styles.alignSelfStart}>{props.title}</Card.Title>
+            <Text style={styles.color_grey}>
+              {moment(props.creationDate).fromNow()}
+            </Text>
+          </View>
+          <View>
+            <Icon name="chevron-right" size={30}/>
+          </View>
         </View>
-        <View>
-          <Icon name="chevron-right" size={30} />
-        </View>
-      </View>
-    </Card>
-  </TouchableOpacity>
-);
+      </Card>
+    </TouchableOpacity>
+  );
+};
 
 export default ReportDetails;
--- src/styles/common.js
+++ src/styles/common.js
@@ -101,6 +101,18 @@ export const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: COLORS.main,
   },
+  report_status_badge: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    margin: 10,
+  },
+  report_status_badge_no_icon: {
+    position: 'absolute',
+    top: -150,
+    right: 0,
+    margin: 10,
+  },
   report_header_image: {
     width: '100%',
     height: 200,
