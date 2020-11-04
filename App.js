db44a5bc3fbb8298004f02e6278cfe7db59958dd
azowyl <facugerstner_29@hotmail.com>
2020-11-04 11:50:26 -0300

    add basic notification config

--- App.js
+++ App.js
@@ -3,6 +3,11 @@ import {NavigationContainer} from '@react-navigation/native';
 import WelcomeScreen from './src/components/screens/welcome/welcome_screen';
 import configureStore from './src/redux/store';
 import {Provider} from 'react-redux';
+import messaging from '@react-native-firebase/messaging';
+
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handled in the background!', remoteMessage);
+});
 
 const App = () => {
   return (
