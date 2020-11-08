b4be706030a4068c7c14433b62b66508b6579370
Azowyl <facugerstner_29@hotmail.com>
2020-11-08 15:11:20 -0300

    down case categories

--- src/components/common/filters/category_filter.js
+++ src/components/common/filters/category_filter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {List, Checkbox} from 'react-native-paper';
+import {upperFirst, toLower} from 'lodash';
 
 const CategoryFilter = (props) => {
   const {selected, onSelect, values} = props;
@@ -9,7 +10,7 @@ const CategoryFilter = (props) => {
       {values.map((category) => (
         <List.Item
           key={category}
-          title={category}
+          title={upperFirst(toLower(category))}
           right={() => (
             <Checkbox
               status={selected.includes(category) ? 'checked' : 'unchecked'}
