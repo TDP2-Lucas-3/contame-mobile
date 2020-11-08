0fe7c2d39bba8bf244b0586ef1adb26b6f7c7fd1
Lucas Lavandeira <llavandeira@devartis.com>
2020-11-08 20:21:35 -0300

    add default message when no reports exist

--- src/components/screens/reports/list/reports_list.js
+++ src/components/screens/reports/list/reports_list.js
@@ -1,32 +1,41 @@
 import React from 'react';
-import {FlatList, View} from 'react-native';
+import {FlatList, Text, View} from 'react-native';
 import {Icon} from 'react-native-elements';
 import {getMyReports} from '../../../../config/routes';
 import ReportDetails from './report_details';
 import useAxios from 'axios-hooks';
 import {styles} from '../../../../styles/common';
 import moment from 'moment';
+import Loading from '../../../common/loading';
 
 const ReportsList = ({navigation}) => {
   const [{data, loading}, refetch] = useAxios(getMyReports());
 
-  return (
+  return loading ? (
+    <Loading />
+  ) : (
     <View style={styles.container}>
-      <FlatList
-        data={(data || []).sort(
-          (a, b) => moment(a.creationDate) < moment(b.creationDate),
-        )}
-        renderItem={({item}) => (
-          <ReportDetails
-            {...item}
-            onPress={() =>
-              navigation.navigate('ReportDetails', {reportId: item.id})
-            }
-          />
-        )}
-        refreshing={loading}
-        onRefresh={refetch}
-      />
+      {data && data.length > 0 ? (
+        <FlatList
+          data={(data || []).sort(
+            (a, b) => moment(a.creationDate) < moment(b.creationDate),
+          )}
+          renderItem={({item}) => (
+            <ReportDetails
+              {...item}
+              onPress={() =>
+                navigation.navigate('ReportDetails', {reportId: item.id})
+              }
+            />
+          )}
+          refreshing={loading}
+          onRefresh={refetch}
+        />
+      ) : (
+        <Text style={styles.alignCenter}>
+          Aún no cargaste ninguna incidencia.
+        </Text>
+      )}
       <Icon
         name="plus"
         type="font-awesome-5"
