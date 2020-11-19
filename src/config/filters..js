9b545812abcce6844737e2ad0a69b5a851904add
Azowyl <facugerstner_29@hotmail.com>
2020-11-19 17:59:56 -0300

    refactor filter system

---
+++ src/config/filters..js
@@ -0,0 +1,12 @@
+export const reportAttributesToFilterBy = [
+  {
+    label: 'Barrio',
+    accesor: 'hood',
+    stateKey: 'neighborhoods',
+  },
+  {
+    label: 'Category',
+    accesor: 'category.name',
+    stateKey: 'categories',
+  },
+];
