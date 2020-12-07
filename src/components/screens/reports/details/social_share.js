fbbc75723b8262a665e611767b2d090558c8ed67
Azowyl <facugerstner_29@hotmail.com>
2020-12-07 11:42:27 -0300

    add menu logic

--- src/components/screens/reports/details/social_share.js
+++ src/components/screens/reports/details/social_share.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View} from 'react-native';
 import {Icon} from 'react-native-elements';
+import {Menu} from 'react-native-paper';
 
 const SocialShare = (props) => {
+  const [visible, setVisible] = useState(false);
+  const shareIcon = (
+    <Icon
+      name="share-square"
+      type="font-awesome-5"
+      color="white"
+      onPress={() => setVisible(!visible)}
+    />
+  );
+
   return (
     <View style={props.containerStyle}>
-      <Icon name="share-square" type="font-awesome-5" color="white" />
+      <Menu
+        anchor={shareIcon}
+        visible={visible}
+        onDismiss={() => setVisible(false)}>
+        <Menu.Item title="Instagram" icon="instagram" />
+        <Menu.Item title="Facebook" icon="facebook" />
+      </Menu>
     </View>
   );
 };
